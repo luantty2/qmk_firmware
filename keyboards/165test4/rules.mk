12eac95f990dbb8951dbb0092284b32274ad1921
weimao <luanty@163.com>
2022-09-17 15:25:48 +0800

    add 165test4 which is based on blackpill(AT32)

---
+++ keyboards/165test4/rules.mk
@@ -0,0 +1,7 @@
+# This file intentionally left blank
+CUSTOM_MATRIX = lite
+QUANTUM_LIB_SRC+= spi_master.c
+# BOARD = BLACKPILL_STM32_F411
+DEBOUNCE_TYPE = asym_eager_defer_pk
+
+EEPROM_DRIVER = transient
