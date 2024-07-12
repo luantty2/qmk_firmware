2ddd1658236d1d3165a4d7ad39129a029300a586
weimao <luanty@163.com>
2024-07-12 14:44:43 +0800

    add elysium test

--- keyboards/elysium/mcuconf.h
+++ keyboards/elysium/mcuconf.h
@@ -5,6 +5,9 @@
 
 #include_next <mcuconf.h>
 
+#undef STM32_PLLM_VALUE
+#define STM32_PLLM_VALUE                    8
+
 #undef STM32_USB_USE_OTG1
 #define STM32_USB_USE_OTG1                  FALSE
 #undef STM32_USB_USE_OTG2
