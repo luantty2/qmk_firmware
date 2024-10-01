1e5ba31acf50256b813edae45675c3f18f834559
weimao <luanty@163.com>
2024-10-01 21:53:48 +0800

    modify unpair keys to numlock

--- keyboards/ankh/keymaps/via/keymap.c
+++ keyboards/ankh/keymaps/via/keymap.c
@@ -12,17 +12,17 @@ enum keycodes{
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_BSPC,  KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
         KC_P4,    KC_P5,    KC_P6,
         KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         KC_P0,    LT(1, KC_PDOT)
     ),
     [1] = LAYOUT(
-        KC_NUM,   _______,  _______,  _______,
+        BT_UNPR,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,
         _______,  _______,  _______,
-        RGB_TOG,  RGB_MOD,  _______,  BT_UNPR,
+        RGB_TOG,  RGB_MOD,  _______,  _______,
         _______,  _______
     ),
 };
